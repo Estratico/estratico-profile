03f32a202f89867febdba5809ce4cdab9960db51
Michael West <michaelwesttdev@gmail.com>
2026-02-02 00:17:38 +0200

    fixed suspence error caused by useSearchParams in contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from "next"
-import { siteConfig } from "@/config/site"
-import { ContactPageContent } from "@/components/pages/contact-content"
-
+import type { Metadata } from "next";
+import { siteConfig } from "@/config/site";
+import { ContactPageContent } from "@/components/pages/contact-content";
+import { connection } from "next/server";
 export const metadata: Metadata = {
   title: "Contact Us",
   description: `Get in touch with ${siteConfig.name}. Let's discuss your project and explore how we can help transform your business with innovative technology solutions.`,
@@ -12,8 +12,9 @@ export const metadata: Metadata = {
   alternates: {
     canonical: `${siteConfig.url}/contact`,
   },
-}
+};
 
-export default function ContactPage() {
-  return <ContactPageContent />
+export default async function ContactPage() {
+  await connection();
+  return <ContactPageContent />;
 }
